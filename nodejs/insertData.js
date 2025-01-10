f3c49c7a718473e2e48fd9bb93b47d990facc535
hangyeoldora <hg14699@naver.com>
2025-01-10 17:57:38 +0900

    [UPDATE] client import 변경 및 불필요 주석 삭제, modelName 환경변수로 변경

--- nodejs/insertData.js
+++ nodejs/insertData.js
@@ -1,43 +1,20 @@
+const { getContainer } = require("./config/client");
 const { PDFLoader } = require("@langchain/community/document_loaders/fs/pdf");
-const path = require("path");
 const { AzureOpenAIEmbeddings } = require("@langchain/openai");
+const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
-/**
- * container 반환
- * @returns {object} container
- */
-const getContainer = async (client) => {
-  // 특정 DB 선택
-  const { database } = await client.databases.createIfNotExists({
-    id: "cosmicworks",
-    partitionKey: {
-      paths: ["/partitionKey"],
-    },
-  });
-  // 특정 컨테이너 (테이블) 선택
-  const { container } = await database.containers.createIfNotExists({
-    id: "vectorTest",
-    partitionKey: {
-      paths: ["/partitionKey"],
-    },
-  });
-
-  return container;
-};
-  
 /**
  * 컨테이너에 item 저장
  * @param {*} container 
  * @param {*} param1 
  */
 const upsert2Container = async (container, {title, content, loc, contentVector}) => {
-  // id - uuid                           
-  const uuidRes = await uuidv4();
+  const uuidRes = await uuidv4(); // id를 위한 uuid 생성
 
   const item = {
     id: uuidRes,
-    partitionKey: "test",
+    filename: 'skelter',
     title,
     content,
     loc, // pdf 페이지 번호
@@ -50,26 +27,25 @@ const upsert2Container = async (container, {title, content, loc, contentVector})
 
 /**
  * 데이터 저장
- * @param {object} client 
+ * @param {object} cosmosClient 
  * @returns 
  */
-const storeDataInDatabase = async (client) => {
+const storeDataInDatabase = async (cosmosClient) => {
   try {
-    const container = await getContainer(client);
-    console.log("Hello, Azure Cosmos DB!");
+    const container = await getContainer(cosmosClient);
     // pdf 로드
     const pdf_path = path.join(__dirname, "./labs.pdf");
     const pdfLoader = new PDFLoader(pdf_path);
     const result = await pdfLoader.load();
 
-    // embedding 모델 표현
+    // embedding 모델 객체 생성
     const embeddings = new AzureOpenAIEmbeddings({
       azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
       azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
       azureOpenAIApiEmbeddingsDeploymentName:
         process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME,
       azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
-      modelName: "text-embedding-3-small",
+      modelName: process.env.AZURE_OPENAI_EMBEDDINGS_MODEL_NAME,
       maxRetries: 3,
     });
 
@@ -93,12 +69,12 @@ const storeDataInDatabase = async (client) => {
 };
 
 /**
- * main 함수
- * @param {object} client
+ * 메인 실행 함수
  */
-async function main(client) {
-  const res = await storeDataInDatabase(client);
+async function main() {
+  const res = await storeDataInDatabase();
   console.log('insert data result: ', res);
+  console.log('저장 로직 실행')
 }
 
 module.exports = main;
